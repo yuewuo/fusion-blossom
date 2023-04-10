97f7237363c1d24743d92fb2dd3ff0f2ae03390d
Yue Wu <yue.wu@yale.edu>
2023-04-10 11:58:42 -0400

    support rotated partition in cli

--- src/cli.rs
+++ src/cli.rs
@@ -190,6 +190,8 @@ pub enum PartitionStrategy {
     CodeCapacityRepetitionCodePartitionHalf,
     /// partition a phenomenological (or circuit-level) planar code with time axis
     PhenomenologicalPlanarCodeTimePartition,
+    /// partition a phenomenological (or circuit-level) rotated code with time axis
+    PhenomenologicalRotatedCodeTimePartition,
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Serialize, Debug)]
@@ -561,6 +563,24 @@ impl PartitionStrategy {
                 }
                 if !config.is_empty() { panic!("unknown config keys: {:?}", config.keys().collect::<Vec<&String>>()); }
                 PhenomenologicalPlanarCodeTimePartition::new_tree(d, noisy_measurements, partition_num
+                        , enable_tree_fusion, maximum_tree_leaf_size).build_apply(code)
+            },
+            Self::PhenomenologicalRotatedCodeTimePartition => {
+                let config = partition_config.as_object_mut().expect("config must be JSON object");
+                let mut partition_num = 10;
+                let mut enable_tree_fusion = false;
+                let mut maximum_tree_leaf_size = usize::MAX;
+                if let Some(value) = config.remove("partition_num") {
+                    partition_num = value.as_u64().expect("partition_num: usize") as usize;
+                }
+                if let Some(value) = config.remove("enable_tree_fusion") {
+                    enable_tree_fusion = value.as_bool().expect("enable_tree_fusion: bool");
+                }
+                if let Some(value) = config.remove("maximum_tree_leaf_size") {
+                    maximum_tree_leaf_size = value.as_u64().expect("maximum_tree_leaf_size: usize") as usize;
+                }
+                if !config.is_empty() { panic!("unknown config keys: {:?}", config.keys().collect::<Vec<&String>>()); }
+                PhenomenologicalRotatedCodeTimePartition::new_tree(d, noisy_measurements, partition_num
                         , enable_tree_fusion, maximum_tree_leaf_size).build_apply(code)
             },
         };
