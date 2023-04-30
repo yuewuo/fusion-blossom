21b60e86d10dd5c74b2fc16c335aa7351ba9e4df
Yue Wu <yue.wu@yale.edu>
2023-04-29 20:43:51 -0400

    add scripts

--- benchmark/paper_fusion_circuit_level/pymatching_compare_various_T/run_fusion.py
+++ benchmark/paper_fusion_circuit_level/pymatching_compare_various_T/run_fusion.py
@@ -34,12 +34,18 @@ for noisy_measurements in noisy_measurements_vec:
     if os.path.exists(syndrome_file_path):
         print("[warning] use existing syndrome data (if you think it's stale, delete it and rerun)")
     else:
-        command = fusion_blossom_qecp_generate_command(d=d, p=p, total_rounds=total_rounds, noisy_measurements=noisy_measurements)
+        if noisy_measurements < min(d, 4):
+            command = fusion_blossom_qecp_generate_command(d=d, p=p, total_rounds=total_rounds, noisy_measurements=noisy_measurements)
+        else:
+            command = fusion_blossom_qecp_generate_command(d=d, p=p, total_rounds=total_rounds, noisy_measurements=min(d, 4))
+            command += ["--use-compact-simulator", "--use-compact-simulator-compressed"]
+            command += ["--simulator-compact-extender-noisy-measurements", f"{noisy_measurements}"]
         command += ["--code-type", "rotated-planar-code"]
         command += ["--noise-model", "stim-noise-model"]
+        command += ["--decoder", "fusion", "--decoder-config", '{"only_stab_z":true,"use_combined_probability":false,"skip_decoding":true}']
+        command += ["--debug-print", "fusion-blossom-syndrome-file", "--fusion-blossom-syndrome-export-filename", syndrome_file_path]
         command += ["--parallel", "0"]  # use all cores
         command += ["--use-brief-edge"]  # to save memory; it takes 23GB to run d=97 and 15min to initialize it...
-        command += ["--fusion-blossom-syndrome-export-config", f'{{"filename":"{syndrome_file_path}","only_stab_z":true,"use_combined_probability":false}}']
         print(command)
         stdout, returncode = run_command_get_stdout(command)
         print("\n" + stdout)
